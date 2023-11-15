dc0e0f0b36c2a7ccffdf559ad7dd044dd9737dcf
pobab <43501409+pobab@users.noreply.github.com>
2023-11-15 16:03:10 +0800

    try teleport player to spawn on first login

---
+++ src/main/java/id/pobab/spawnprotect/events/EventPlayerLogin.java
@@ -0,0 +1,22 @@
+package id.pobab.spawnprotect.events;
+
+import com.mojang.logging.LogUtils;
+import id.pobab.spawnprotect.SpawnProtect;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod.EventBusSubscriber(modid = SpawnProtect.MODID)
+public class EventPlayerLogin {
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public static void onPlayerFirstLogin(PlayerEvent.PlayerLoggedInEvent event) {
+        Player player = event.getEntity();
+        ServerPlayer serverPlayer = new ServerPlayer(player.getServer(), player.getServer().getLevel(Level.OVERWORLD), player.getGameProfile());
+        LOGGER.info(serverPlayer.getStats().toString());
+    }
+}
