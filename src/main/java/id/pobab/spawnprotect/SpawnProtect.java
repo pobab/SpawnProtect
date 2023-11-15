1f9acab862d5df238ee0f3e9b4e5f3dbecde1dc4
pobab <43501409+pobab@users.noreply.github.com>
2023-11-15 17:09:51 +0800

    clean up Import

--- src/main/java/id/pobab/spawnprotect/SpawnProtect.java
+++ src/main/java/id/pobab/spawnprotect/SpawnProtect.java
@@ -1,23 +1,17 @@
 package id.pobab.spawnprotect;
 
 import com.mojang.logging.LogUtils;
-import net.minecraft.core.Registry;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.saveddata.SavedData;
 import net.minecraft.world.phys.Vec3;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.ForgeRegistries;
 import org.slf4j.Logger;
 
 @Mod(SpawnProtect.MODID)
