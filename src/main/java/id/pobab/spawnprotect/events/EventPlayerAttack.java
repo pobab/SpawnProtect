1ce312277580ab19b39b2db6e6a129358f154486
pobab <43501409+pobab@users.noreply.github.com>
2023-11-20 11:04:42 +0800

    register `AttackEntityEvent`

---
+++ src/main/java/id/pobab/spawnprotect/events/EventPlayerAttack.java
@@ -0,0 +1,24 @@
+package id.pobab.spawnprotect.events;
+
+import com.mojang.logging.LogUtils;
+import id.pobab.spawnprotect.SpawnProtect;
+import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.event.entity.player.AttackEntityEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+
+@Mod.EventBusSubscriber(modid = SpawnProtect.MODID)
+public class EventPlayerAttack {
+    private static final Logger LOGGER = LogUtils.getLogger();
+    static int range = 300;
+
+    @SubscribeEvent
+    public static void onPlayerBreaksBlock(AttackEntityEvent event) {
+        Player player = event.getEntity();
+        int x = player.getBlockX();
+        int z = player.getBlockZ();
+        if (!(x <= range && z <= range && x >= -range && z >= -range)) return;
+        event.setCanceled(true);
+    }
+}
